d6065ed2e1859a2fa543d1d3e681389a61e625a8
MuhammadWaleedMalik <148714412+MuhammadWaleedMalik@users.noreply.github.com>
2024-09-27 18:18:53 +0500

    add new routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.contoller.js";
+import { 
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    registerUser,
+    changeCurrentPasword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+     } from "../controllers/user.contoller.js";
 
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -25,6 +37,13 @@ router.route("/register").post(
 router.route('/login').post(loginUser)
 router.route('/logout').post(verifyJWT, logoutUser)
 router.route('/refreshToken').post(refreshAccessToken)
+router.route('/changePassword').post(verifyJWT,changeCurrentPasword)
+router.route('/current-user').get(verifyJWT,getCurrentUser)
+router.route('/updateAccountDetails').patch(verifyJWT,updateAccountDetails)
+router.route('/updateAvatar').patch(verifyJWT,upload.single('avatar'),updateUserAvatar)
+router.route('/updateCoverImage').patch(verifyJWT,upload.single('coverImage'),updateUserCoverImage)
+router.route('/getUserProfile/:user').get(verifyJWT,getUserChannelProfile)
+router.route('/watchHistory').get(verifyJWT,getWatchHistory)
 
 
 
