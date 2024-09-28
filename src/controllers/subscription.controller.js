bbe83c883b1772b012e12b6e09062e33e4881d94
MuhammadWaleedMalik <148714412+MuhammadWaleedMalik@users.noreply.github.com>
2024-09-29 01:56:45 +0500

    tweets and subscription done

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -6,20 +6,85 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 
-const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-    // TODO: toggle subscription
-})
-
-// controller to return subscriber list of a channel
-const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-})
+const toggleSubscription = async (req, res) => {
+    const { channelId } = req.params;
+    const userId = req.user._id; 
+  
+    try {
+      const subscription = await Subscription.findOne({ channel: channelId, subscriber: userId });
+  
+      if (subscription) {
+        // Unsubscribe
+        await subscription.remove();
+        return res.status(200).json(new ApiResponse(200,'USER UNSUBSCRIBED SUCCESFULLY'));
+      } else {
+        // Subscribe
+        const newSubscription = await Subscription.create({ channel: channelId, subscriber: userId });
+        return res.status(201).json(new ApiResponse(200, newSubscription  , "Subscribed to channel successfully" ));
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(new ApiResponse(500, "Error toggling subscription" ));
+    }
+  };
+    
+    // controller to return subscriber list of a channel
+    const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+        const { channelId } = req.params;
+      
+        if (!channelId) {
+          throw new ApiError(400, "You Failed to send Channel ID");
+        }
+      
+        const result = await Subscription.aggregate([
+          {
+            $match: { channel: mongoose.Types.ObjectId(channelId) }
+          },
+          {
+            $facet: {
+              count: [{ $count: "count" }],
+              subscribers: [{ $skip: 0 }] // or use $limit if you want to limit the number of subscribers
+            }
+          }
+        ]);
+      
+        if (!result || result.length === 0) {
+          throw new ApiError(500, "Failed TO Fetch Subscribers");
+        }
+      
+        const subscriberCount = result[0].count[0].count;
+        const subscribers = result[0].subscribers;
+      
+        return res.status(200).json(new ApiResponse(200, { count: subscriberCount, subscribers }, "Subscribers fetched Successfully"));
+      });
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
-})
+    const { user } = req.user;
+  
+    const result = await Subscription.aggregate([
+      {
+        $match: {
+          subscriber: mongoose.Types.ObjectId(user._id)
+        }
+      },
+      {
+        $facet: {
+          count: [{ $count: "count" }],
+          channels: [{ $lookup: { from: "users", localField: "channel", foreignField: "_id", as: "channel" } }]
+        }
+      }
+    ]);
+  
+    if (!result || result.length === 0) {
+      throw new ApiError(404, "No subscribed channels found");
+    }
+  
+    const subscribedChannelCount = result[0].count[0].count;
+    const subscribedChannels = result[0].channels.map(channel => channel.channel[0]);
+  
+    return res.status(200).json(new ApiResponse(200, { count: subscribedChannelCount, channels: subscribedChannels }, "Subscribed channels fetched successfully"));
+  });
 
 export {
     toggleSubscription,
--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -6,19 +6,77 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
+    const {content} =req.body
+    const {user}=req.user
+
+    if(!content){
+        throw new Error(400,'Please Enter Content for Tweet')
+    }
+
+    if(user){
+        throw new ApiError(400,'Error User Is Not Veries')
+    }
+
+    const tweet = await Tweet.create({
+        content,
+        owner:user._id
+    })
+
+    return
+    res
+    .status(200)
+    .json(new ApiResponse(200,tweet,"Tweet Added Succesfully"))
+
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
+    const user=req.user
+    const tweets = await Tweet.find({owner:user._id}).sort({createdAt:-1})
+    return 
+    res
+    .status(200,tweets,'Tweets Fetched seccesfully')
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
+    const {tweet_id}=req.params
+    const {content} =req.body
+    
+    if(!tweet_id){
+        throw new ApiError(400,"You failed To Send The tweet ID")
+    }
+    if(!content){
+        throw new Error(400,'Please Enter Content for Tweet')
+    }
+
+    const newTweet = await Tweet.findOneAndUpdate(tweet_id,
+        {
+            $set:{content}
+        },
+        {new:true}
+    )
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,newTweet,"tweet Modified Succesfuly"))
+
+    
+
+
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
+    const {tweet_id} = req.params
+    if(!tweet_id){
+        throw new ApiError(400,"You failed To Send The tweet ID")
+    }
+    const tweet = await Tweet.findByIdAndDelete(tweet_id)
+    if(!tweet){
+        throw new ApiError(404,"Tweet Not Found")
+        }
+        return res
+        .status(200)
+        .json(new ApiResponse(200,tweet,"Tweet Deleted Succesfuly"))
+
 })
 
 export {
